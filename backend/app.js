6d6ccd159b122585ca1064c9d437ac1d3ccf31a9
Wahhaj <wahhajk9@gmail.com>
2025-07-19 20:01:24 +0500

    Middleware setup comitted successfully

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app=express()
+
+dotenv.config({path:'./config/config.env'})
+
+app.use(cors({
+    origin:["process.env.FRONTEND_URL"],
+    methods:["POST"],
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/hello",(req,res)=>{
+    return res.status(200).json({
+        success:true,
+        message:"Hello Created successfully"
+    })
+})
+
+
+
+export default app;

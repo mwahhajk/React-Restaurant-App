009cdee1af15b8dae3a0e8185f6611703334cdb5
Wahhaj <wahhajk9@gmail.com>
2025-08-05 03:34:46 +0500

    Complete backend committed

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -1,5 +1,24 @@
-export const sendReservation=(req,res)=>{
+import { Reservation } from "../models/reservationSchema.js";
+
+export const sendReservation=async(req,res)=>{
     console.log("Send Reservation from Reservation Controller");
-    
-    
+
+    const{firstName,lastName,phone,email,date,time}=req.body;
+    console.log(firstName,lastName,phone,email,date,time);
+
+    if(!firstName||!lastName||!phone||!email ||!date ||!time)
+    {
+        return res.status(400).json({message:"Please provide all required fields"})
+    }
+
+    const createdReservation=await Reservation.create(req.body)
+
+    if(createdReservation)
+    {
+        return res.status(201).json({
+            success:true,
+            message:"Reservation Created successfully",
+            createdReservation
+        })
+    }
 }
